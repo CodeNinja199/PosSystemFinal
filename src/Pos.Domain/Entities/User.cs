fc694e7916cb5c88168257cd66cfce67531ecf08
Haiqa Usman <haiqausman199@gmail.com>
2026-08-17 19:18:50 -0700

    Add the User entity with a password hash and a role

---
+++ src/Pos.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+using Pos.Domain.Enums;
+
+namespace Pos.Domain.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = string.Empty;
+
+    public UserRole Role { get; set; }
+
+    public DateTime RegisteredAt { get; set; }
+}
