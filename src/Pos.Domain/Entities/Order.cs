29ca62aa301dd113f2b61c5a29746b7ebb8456aa
Haiqa Usman <haiqausman199@gmail.com>
2026-08-17 19:35:51 -0700

    Add the Order entity with status, payment method, and total

---
+++ src/Pos.Domain/Entities/Order.cs
@@ -0,0 +1,18 @@
+using Pos.Domain.Enums;
+
+namespace Pos.Domain.Entities;
+
+public class Order
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public DateTime PlacedAt { get; set; }
+
+    public OrderStatus Status { get; set; }
+
+    public PaymentMethod PaymentMethod { get; set; }
+
+    public decimal Total { get; set; }
+}
